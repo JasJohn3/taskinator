7f8e161513662741fea64f48d00d1f48b50ccb45
Your Name <jasjohn3@uat.edu>
2021-06-29 15:34:15 -0700

    Add task ability complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,14 @@
 let buttonEL = document.getElementById('save-task');
+let tasksToDoEl = document.querySelector("#tasks-to-do");
 
+buttonEL.addEventListener('click', createTaskHandler);
 
-buttonEL.addEventListener('click', addTask);
-
-function addTask(){
-  let tasksToDoEl = document.querySelector("#tasks-to-do");
+function createTaskHandler(){
   let taskItemEL = document.createElement('li');
   taskItemEL.className="task-item";
   taskItemEL.textContent ="New Task Item!";
   let deleteButton = document.createElement('button')
   deleteButton.className = "btn";
   deleteButton.innerHTML = "Delete";
-  
   tasksToDoEl.appendChild(taskItemEL);
-
 };
