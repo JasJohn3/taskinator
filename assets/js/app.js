e2a9b1706eff4d2e56f6e6031b3d1fa0ec325f3a
Your Name <jasjohn3@uat.edu>
2021-06-29 15:26:51 -0700

    Created first initial insert item for Add Task

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,5 +4,14 @@ let buttonEL = document.getElementById('save-task');
 buttonEL.addEventListener('click', addTask);
 
 function addTask(){
-  alert('Your Button Works!');
+  let tasksToDoEl = document.querySelector("#tasks-to-do");
+  let taskItemEL = document.createElement('li');
+  taskItemEL.className="task-item";
+  taskItemEL.textContent ="New Task Item!";
+  let deleteButton = document.createElement('button')
+  deleteButton.className = "btn";
+  deleteButton.innerHTML = "Delete";
+  
+  tasksToDoEl.appendChild(taskItemEL);
+
 };
