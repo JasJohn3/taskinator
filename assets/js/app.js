8ba03f5332d5e782eeb95fec765d909913fd1d4a
Your Name <jasjohn3@uat.edu>
2021-06-30 12:52:12 -0700

    Styling changes to CSS for personalization

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,6 +7,10 @@ function taskFormHandler(event) {
   event.preventDefault(); 
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  if(!taskNameInput || !taskTypeInput){
+    alert("You need to fill out the task form!")
+    return false;
+  }
   let taskDataObject ={
     name: taskNameInput,
     type: taskTypeInput
