ee3ac8be120c95f277cb18d6cddf09fd9b7cd225
Your Name <jasjohn3@uat.edu>
2021-06-30 12:28:39 -0700

    Created a function to generate li HTML Element and convert user date to an object

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,33 @@
-var formEL = document.querySelector('#task-form');
-var tasksToDoEl = document.querySelector("#tasks-to-do");
+var formElement = document.querySelector('#task-form');
+var tasksToDoElement = document.querySelector("#tasks-to-do");
 
 
 
-function createTaskHandler(event) { 
+function taskFormHandler(event) { 
   event.preventDefault(); 
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
-  // create a li element for the unordered list
-  var listItemEl = document.createElement("li");
-  // apply a class name for css styling 
-  listItemEl.className = "task-item";
-  // create div to hold task info and add to list item
-  var taskInfoEl = document.createElement('div');
-  // class name for newly created div
-  taskInfoEl.className ='task-info';
-  // add HTML content to div using string literal format.
-  taskInfoEl.innerHTML = `<h3 class='task-name'>${taskNameInput}</h3><span class='task-type'>${taskTypeInput}</span>`;
-
-  //  append list item with the task info inner html
-  listItemEl.appendChild(taskInfoEl); 
-  // append the li to the ul element
-  tasksToDoEl.appendChild(listItemEl); 
+  let taskDataObject ={
+    name: taskNameInput,
+    type: taskTypeInput
+  }
+  createTaskElement(taskDataObject);
   }; 
-
-  formEL.addEventListener("submit", createTaskHandler);
+  function createTaskElement(taskDataObject){
+    // create a li element for the unordered list
+    var listItemElement = document.createElement("li");
+    // apply a class name for css styling 
+    listItemElement.className = "task-item";
+    // create div to hold task info and add to list item
+    var taskInfoElement = document.createElement('div');
+    // class name for newly created div
+    taskInfoElement.className ='task-info';
+    // add HTML content to div using string literal format.
+    taskInfoElement.innerHTML = `<h3 class='task-name'>${taskDataObject.name}</h3><span class='task-type'>${taskDataObject.type}</span>`;
+  
+    //  append list item with the task info inner html
+    listItemElement.appendChild(taskInfoElement); 
+    // append the li to the ul element
+    tasksToDoElement.appendChild(listItemElement); 
+  }
+  formElement.addEventListener("submit", taskFormHandler);
