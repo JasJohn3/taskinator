427095779bda9dfc23c2056f8064a9035af86455
Your Name <jasjohn3@uat.edu>
2021-06-30 12:13:22 -0700

    Added innerHTML element to the list item that utilizes user input values

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,12 +3,24 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
 
-var createTaskHandler = function(event) { 
+function createTaskHandler(event) { 
   event.preventDefault(); 
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  // create a li element for the unordered list
+  var listItemEl = document.createElement("li");
+  // apply a class name for css styling 
+  listItemEl.className = "task-item";
+  // create div to hold task info and add to list item
+  var taskInfoEl = document.createElement('div');
+  // class name for newly created div
+  taskInfoEl.className ='task-info';
+  // add HTML content to div using string literal format.
+  taskInfoEl.innerHTML = `<h3 class='task-name'>${taskNameInput}</h3><span class='task-type'>${taskTypeInput}</span>`;
 
-  var listItemEl = document.createElement("li"); 
-  listItemEl.className = "task-item"; 
-  listItemEl.textContent = "This is a new task."; 
+  //  append list item with the task info inner html
+  listItemEl.appendChild(taskInfoEl); 
+  // append the li to the ul element
   tasksToDoEl.appendChild(listItemEl); 
   }; 
 
